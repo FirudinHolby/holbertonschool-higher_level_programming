192eef4efe247a78590960d7acf0344ca999992f
FirudinHolby <12738@holbertonstudents.com>
2025-12-14 07:41:27 +0000

    task3.3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
 def common_elements(set_1, set_2):
-    newset = set_1.intersection(set_2)
-    return new_list
+    common = set()
+
+    for item in set_1:
+        if item in set_2:
+            common.add(item)
+
+    return common
